0f78a1acc4aa046b2fe02528d0dd500d1656c5dc
ramyamounir <44840015+ramyamounir@users.noreply.github.com>
2025-06-04 19:10:44 -0700

    feat: add kill9 script

--- config.h
+++ config.h
@@ -242,6 +242,7 @@ static const Key keys[] = {
 	{ MODKEY|ShiftMask,                     XK_p,           spawn,                    SHCMD("maimpick") },                               // open screenshot picker
 	{ MODKEY,                               XK_m,           spawn,                    SHCMD("dmenumount") },                             // open mounting menu
 	{ MODKEY|ShiftMask,                     XK_m,           spawn,                    SHCMD("dmenuumount") },                            // open unmounting menu
+	{ MODKEY,                               XK_Delete,      spawn,                    SHCMD("kill9") },                                  // Kill -9 script in fzf
 	{ MODKEY,                               XK_n,           spawn,                    SHCMD("redshift -O 4500 -b 0.8") },                // Add redshift effect (cumulative)
 	{ MODKEY|ShiftMask,                     XK_n,           spawn,                    SHCMD("redshift -x") },                            // Reset redshift
 	{ MODKEY,                               XK_BackSpace,	spawn,                    SHCMD("sysact") },                                 // Script for logging out
